59510f09f71dec871a9c25e688e2bc95c00bf5a9
KevinLi19 <52968134+KevinLi19@users.noreply.github.com>
2023-05-08 19:21:34 -0400

    Initial commit

---
+++ src/FoodMenu.java
@@ -0,0 +1,3 @@
+public class FoodMenu {
+    public String Name ="Rice";
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        FoodMenu foodMenu = new FoodMenu();
+
+        System.out.println("Food name is: " + foodMenu.Name);
+    }
+}
